b91b28511edf11dbf9bcfe65d2b8e11933b88d49
Santiago <santipahur@gmail.com>
2024-07-10 08:09:10 -0500

    final

--- src/public/js/settingAdmin.js
+++ src/public/js/settingAdmin.js
@@ -5,8 +5,6 @@ const menu = document.querySelector(".menu");
 const contenedorContenido = document.querySelector(".contenedor_contenido");
 const barraUsuario = document.querySelector(".barra_usuario");
 
-
-
 const fotoUsuario = document.querySelector(".foto_usuario");
 const eleccionUsuario2 = document.querySelector(".eleccion_usuario2");
 
@@ -14,116 +12,116 @@ const eleccionUsuario2 = document.querySelector(".eleccion_usuario2");
 const eleccionUsuario = document.querySelector(".eleccion_usuario");
 const fotoUsuario2 = document.querySelector(".foto_usuario2");
 
-
-
-
 // RESPONSIVE ELECCION DE CERRA SESION Y EDITA PERFIL
 fotoUsuario2.addEventListener("click", () => {
-    eleccionUsuario.classList.toggle("aparece");
-
-})
+  eleccionUsuario.classList.toggle("aparece");
+});
 
 // Ocultar el menú si se hace clic fuera de él RESPONSIVE
 document.addEventListener("click", (event) => {
-    const isClickInside = eleccionUsuario.contains(event.target) || fotoUsuario2.contains(event.target);
-    // Comprueba si el elemento en el que se hizo clic (event.target) está contenido dentro del div (eleccion_usuario2 o fotoUsuario)
-    // ||, si el clic ocurrió dentro de cualquiera de estos elementos, isClickInside será TRUE.
-    //  contains, se usa para determinar si el elemento en el que se hizo clic (event.target) es un descendiente del div con la clase.
+  const isClickInside =
+    eleccionUsuario.contains(event.target) ||
+    fotoUsuario2.contains(event.target);
+  // Comprueba si el elemento en el que se hizo clic (event.target) está contenido dentro del div (eleccion_usuario2 o fotoUsuario)
+  // ||, si el clic ocurrió dentro de cualquiera de estos elementos, isClickInside será TRUE.
+  //  contains, se usa para determinar si el elemento en el que se hizo clic (event.target) es un descendiente del div con la clase.
 
-    if (!isClickInside) { // si es diferente a TRUE, significa que el clic no ocurrio dentro del div
-        eleccionUsuario.classList.remove("aparece");
-    }
+  if (!isClickInside) {
+    // si es diferente a TRUE, significa que el clic no ocurrio dentro del div
+    eleccionUsuario.classList.remove("aparece");
+  }
 });
-
 
 // NORMAL ELECCION DE CERRA SESION Y EDITA PERFIL
 fotoUsuario.addEventListener("click", () => {
-    eleccionUsuario2.classList.toggle("apareceInicial");
-
-})
+  eleccionUsuario2.classList.toggle("apareceInicial");
+});
 
 // Ocultar el menú si se hace clic fuera de él NORMAL
 document.addEventListener("click", (event) => {
-    const isClickInside = eleccionUsuario2.contains(event.target) || fotoUsuario.contains(event.target);
+  const isClickInside =
+    eleccionUsuario2.contains(event.target) ||
+    fotoUsuario.contains(event.target);
 
-    if (!isClickInside) {
-        eleccionUsuario2.classList.remove("apareceInicial");
-    }
+  if (!isClickInside) {
+    eleccionUsuario2.classList.remove("apareceInicial");
+  }
 });
-
-
 
 // MENU RESPONSIVE
 menu.addEventListener("click", () => {
-    // Esta el el RESPOSIVE, vuelva a la posicion
-    barraLateral.classList.toggle("max-barra-lateral");
+  // Esta el el RESPOSIVE, vuelva a la posicion
+  barraLateral.classList.toggle("max-barra-lateral");
 
-    // classList.contains() es una función de JavaScript que se 
-    // utiliza para verificar si un elemento HTML tiene una clase específica
-    if (barraLateral.classList.contains("max-barra-lateral")) {
-        menu.children[0].style.display = "none"; // icon menu
-        menu.children[1].style.display = "block"; //icon circulo
-    }
-    else {
-        menu.children[0].style.display = "block"; // icon menu
-        menu.children[1].style.display = "none"; // icon circulo
-    }
-
+  // classList.contains() es una función de JavaScript que se
+  // utiliza para verificar si un elemento HTML tiene una clase específica
+  if (barraLateral.classList.contains("max-barra-lateral")) {
+    menu.children[0].style.display = "none"; // icon menu
+    menu.children[1].style.display = "block"; //icon circulo
+  } else {
+    menu.children[0].style.display = "block"; // icon menu
+    menu.children[1].style.display = "none"; // icon circulo
+  }
 });
 
 // MENU DESPEGABLE NORMAL VISTA
 menu_icon.addEventListener("click", () => {
-    // Barra de Arriba
-    barraUsuario.classList.toggle("min-barra");
+  // Barra de Arriba
+  barraUsuario.classList.toggle("min-barra");
 
-    // Barra Lateral
-    barraLateral.classList.toggle("mini-barra-lateral");
+  // Barra Lateral
+  barraLateral.classList.toggle("mini-barra-lateral");
 
-    // organizar el margin-left del CONTENIDO. 
-    contenedorContenido.classList.toggle("min-contenido");
+  // organizar el margin-left del CONTENIDO.
+  contenedorContenido.classList.toggle("min-contenido");
 
-
-    // Para todos los span encontrados le agregamos la CALSE .oculto
-    spans.forEach((span) => {
-        span.classList.toggle("oculto");
-    });
+  // Para todos los span encontrados le agregamos la CALSE .oculto
+  spans.forEach((span) => {
+    span.classList.toggle("oculto");
+  });
 });
-
 
 // Abrir documentos
 // REQUISITOS FUNCIONALES
-document.getElementById('openPdfBtnRequisitos').addEventListener('click', function() {
-  var pdfViewer = document.getElementById('pdfViewer');
-  pdfViewer.style.display = 'block';
-  pdfViewer.src = '/plugins/documentacion/.pdf'; 
-});
+document
+  .getElementById("openPdfBtnRequisitos")
+  .addEventListener("click", function () {
+    var pdfViewer = document.getElementById("pdfViewer");
+    pdfViewer.style.display = "block";
+    pdfViewer.src = "/plugins/documentacion/RFs_funcionales.pdf";
+  });
 
 // CASOS DE USO
-document.getElementById('openPdfBtnCasosUso').addEventListener('click', function() {
-  var pdfViewer = document.getElementById('pdfViewer');
-  pdfViewer.style.display = 'block';
-  pdfViewer.src = '/plugins/documentacion/.pdf'; 
-});
+document
+  .getElementById("openPdfBtnCasosUso")
+  .addEventListener("click", function () {
+    var pdfViewer = document.getElementById("pdfViewer");
+    pdfViewer.style.display = "block";
+    pdfViewer.src = "/plugins/documentacion/pitch_samsoft.pdf";
+  });
 
 // MANUAL DE USUARIO
-document.getElementById('openPdfBtnManualUsuario').addEventListener('click', function() {
-  var pdfViewer = document.getElementById('pdfViewer');
-  pdfViewer.style.display = 'block';
-  pdfViewer.src = '/plugins/documentacion/FormularioNuevaHerramienta.pdf'; 
-});
+document
+  .getElementById("openPdfBtnManualUsuario")
+  .addEventListener("click", function () {
+    var pdfViewer = document.getElementById("pdfViewer");
+    pdfViewer.style.display = "block";
+    pdfViewer.src = "/plugins/documentacion/Manual_de_Usuario_SAMSOFT.pdf";
+  });
 
 // MANUAL TECNICO
-document.getElementById('openPdfBtnManualTecnico').addEventListener('click', function() {
-  var pdfViewer = document.getElementById('pdfViewer');
-  pdfViewer.style.display = 'block';
-  pdfViewer.src = '/plugins/documentacion/pitch_samsoft.pdf'; 
-});
-
+document
+  .getElementById("openPdfBtnManualTecnico")
+  .addEventListener("click", function () {
+    var pdfViewer = document.getElementById("pdfViewer");
+    pdfViewer.style.display = "block";
+    pdfViewer.src = "/plugins/documentacion/FinalManualTecnico.pdf";
+  });
 
 // PASAR DE HOJA A HOJA
 const ir = () => {
   window.location.href = "/dash/editarPerfil";
-}
+};
 
 const editarPerfil = () => {
   window.location.href = "/dash/editarPerfil";
@@ -134,34 +132,33 @@ const url = sessionStorage.getItem("urlApi");
 
 // CERRAS SESION
 const cerrarSesion = () => {
-    sessionStorage.setItem("token", "");
-    sessionStorage.setItem("urlApi", "");
-    sessionStorage.setItem("idUser", "");
-    window.location.href = '/login';
+  sessionStorage.setItem("token", "");
+  sessionStorage.setItem("urlApi", "");
+  sessionStorage.setItem("idUser", "");
+  window.location.href = "/login";
 };
 
 // VERIFICAR INGRESO
 const urlComprobar = url + "/api/oauth";
 
 if (token == "" || token == null) {
-  window.location.href = "/login"
-};
+  window.location.href = "/login";
+}
 if (url == "" || url == null) {
-  window.location.href = "/login"
-};
+  window.location.href = "/login";
+}
 
 const options = {
   method: "POST",
   headers: {
-    'Content-Type': 'application/json',
-     'Authorization' : `Bearer ${token}`
-  }
-}
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
 fetch(urlComprobar, options)
-  .then(res => res.json())
-  .then(data => {
+  .then((res) => res.json())
+  .then((data) => {
     if (data.error == true) {
-      window.location.href = "/login"
+      window.location.href = "/login";
     }
   });
-
